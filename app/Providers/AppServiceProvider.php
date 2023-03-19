0407b59678120d054875bf1a2f1068558f0cc732
Matvey <36668268+slmatthew@users.noreply.github.com>
2023-03-20 00:59:22 +0300

    exists_or_null для валидации

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -25,5 +26,20 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrapFive();
+
+        Validator::extend(
+            'exists_or_null',
+            function ($attribute, $value, $parameters)
+            {
+                if(is_null($value) || $value == 0 || $value == '0') {
+                    return true;
+                } else {
+                    $validator = Validator::make([$attribute => $value], [
+                        $attribute => 'exists:' . implode(",", $parameters)
+                    ]);
+                    return !$validator->fails();
+                }
+            }
+        );
     }
 }
